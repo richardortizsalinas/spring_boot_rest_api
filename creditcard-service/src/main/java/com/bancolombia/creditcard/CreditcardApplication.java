24b608623dea3b5f3d7b343a2e02d27f9aa93413
rortiz <rortiz@rortiz-virtual-machine>
2018-03-12 12:03:52 -0700

    ajustes vault

--- creditcard-service/src/main/java/com/bancolombia/creditcard/CreditcardApplication.java
+++ creditcard-service/src/main/java/com/bancolombia/creditcard/CreditcardApplication.java
@@ -1,9 +1,10 @@
 package com.bancolombia.creditcard;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 //@EnableEurekaClient
 @SpringBootApplication
@@ -11,6 +12,13 @@ public class CreditcardApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CreditcardApplication.class, args);
+    }
+    @Value("${spring.datasource.url}")
+    String password;
+
+    @PostConstruct
+    private void postConstruct() {
+        System.out.println("My password is: " + password);
     }
 
 }
