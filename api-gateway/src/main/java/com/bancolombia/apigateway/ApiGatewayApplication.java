70480ee2ad8f2e8c387bcaee52b03800ff0b1acf
richard.ortiz <richard.ortiz@RICHARDORTIZ1.pragma.com.co>
2018-04-16 08:48:04 -0500

    ajustes api gateway
    aplicacion circuit breaker con Hystrix

--- api-gateway/src/main/java/com/bancolombia/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/bancolombia/apigateway/ApiGatewayApplication.java
@@ -3,9 +3,11 @@ package com.bancolombia.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.*;
 
 @SpringBootApplication
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
